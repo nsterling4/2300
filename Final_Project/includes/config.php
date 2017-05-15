b05ca27d61f4441c4a16dd0eaac02518ebc7b1f7
nsterling4 <nas95@cornell.edu>
2017-05-15 14:59:20 -0400

    git fix

---
+++ Final_Project/includes/config.php
@@ -0,0 +1,13 @@
+<?php 
+	// ** MySQL connection settings ** // 
+	// database host
+	define( 'DB_HOST', 'localhost' );
+	// database name - info230_SP17_username 
+	define( 'DB_NAME', 'info230_SP17_fp_snowsport' );
+	// Your MySQL / Course Server username 
+	define('DB_USER', 'fp_snowsport');
+	// ...and password
+	define('DB_PASSWORD', 'SAACwebsite'); 
+?>
+
+
--- Final_Project/includes/scripts.js
+++ Final_Project/includes/scripts.js
@@ -12,15 +12,12 @@
    			// Logged into your app and Facebook.
    			console.log('you been connected')
         console.log(response.authResponse.accessToken);
+        console.log (response.authResponse.userID);
 	    } else {
 	    	console.log('fam u aint connected')
 	    // The person is not logged into this app or we are unable to tell. 
 	    }
 	});	
-
-  function fbcheck(){
-    window.location.reload();
-  }
 
 	function fbLogout() {
         FB.logout(function (response) {
@@ -37,7 +34,3 @@
      js.src = "//connect.facebook.net/en_US/sdk.js";
      fjs.parentNode.insertBefore(js, fjs);
    }(document, 'script', 'facebook-jssdk'));
-
-  function fbcheck(){
-    window.location.reload();
-  }
