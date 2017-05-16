c1310fa9c1a33b5db9d856573062334bbff0d5d4
nsterling4 <nas95@cornell.edu>
2017-05-16 01:59:54 -0400

    email

--- Final_Project/contact.php
+++ Final_Project/contact.php
@@ -28,7 +28,7 @@
 
 		<div class="page_body"> <!--Photo Gallery-->
 			<div class="container">
-				<form method="post" class="forms" action="mail.php">
+				<form method="post" class="forms">
 						Name: <input type="text" name="name">
 						E-mail: <input type="text" name="email"><br><br>
 						What is your affiliation to Cornell?<select name="affiliation"> 
@@ -52,7 +52,47 @@
 
 				</form> 	
 
-		    </div>  <!-- End of container div -->  	   
+		    </div>  <!-- End of container div -->  	 
+
+
+
+		    <?php 
+
+
+
+            // checks if the form has been submitted or not
+          	if(isset($_POST["submit"])) {
+
+                    $name= filter_input( INPUT_POST, trim('name'), FILTER_SANITIZE_STRING );
+                    $email= filter_input( INPUT_POST, trim('email'), FILTER_SANITIZE_STRING );
+                    $affiliation= filter_input( INPUT_POST, trim('affiliation'), FILTER_SANITIZE_STRING );
+                    $involved= filter_input( INPUT_POST, trim('involved'), FILTER_SANITIZE_STRING );
+                    $info= filter_input( INPUT_POST, trim('comment'), FILTER_SANITIZE_STRING );
+
+
+                    echo "Name: $name \n Email: $email \n Affiliation: $affiliation \n $Involved: $involved \n Comment: $info";
+
+
+                        // EDIT THE 2 LINES BELOW AS REQUIRED
+    		$email_to = "nas95@cornell.edu";
+    		$email_subject = "CONTACT US";
+
+
+    		$email_message="Name: $name \n Email: $email \n Affiliation: $affiliation \n $Involved: $involved \n Comment: $info";
+  
+ 
+    // $email_message .= "Name: ".clean_string($name)."\n";
+    // $email_message .= "Email: ".clean_string($email)."\n";
+    // $email_message .= "Affiliation: ".clean_string($affiliation)."\n";
+    // $email_message .= "Involved: ".clean_string($involved)."\n";
+    // $email_message .= "Comments: ".clean_string($info)."\n";
+ 
+// create email headers
+	mail($email_to, $email_subject, $email_message); 
+ 
+      }
+        
+?>  
 
 
 
