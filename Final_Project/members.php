f297299172e29d8e6d97e8291a884b0d5a3f5288
William Lin <William Lin>
2017-05-15 23:47:31 -0400

    added admin members function for removing a member

--- Final_Project/members.php
+++ Final_Project/members.php
@@ -27,9 +27,7 @@
 		<div class="page_body"> <!--Photo Gallery-->
 			<div class="container">
 			<?php
-				if (isset($_SESSION['valid_user'])) {
-					// Membership info displayed in a table
-    			print("<div>");
+				print("<div>");
 
     			$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 			    $member = $mysqli->query("SELECT DISTINCT memberID, first_name, last_name, sport FROM members");
@@ -39,36 +37,63 @@
 			    	$f_name = $name['first_name'];
             		$l_name = $name['last_name'];
             		$sport = $name['sport'];
+            		$year = $name['year'];
             		$photoID = $name['photoID'];
             		$memberID = $name["memberID"];
+            		//get picture info if picture is available
             		if(!empty($photoID)){
             			$file_path = $mysqli->query("SELECT picPath FROM photos WHERE photoID = $photoID");
             			$photo = "<img src='$file_path'alt='$f_name'>";
             		}
+					if (isset($_SESSION['valid_user'])) {
+						// Membership info displayed in a table
+				        print( 
+				            "<table> 
+				                <tr>
+				                	<td> $photo </td>
+				                    <td> $f_name </td>
+				                    <td> $l_name </td> 
+				                    <td> $sport </td>
+				                    <td> $year </td>
+				                    <td> <input type='checkbox' name='option[]'' value='$memberID'> In Attendance? </td>
+				                    <td> <input type='checkbox' name='remove[]' value='$memberID'> Remove Member?</td>
+				                </tr>
+				            </table>");
+					    print("<input type='submit' name='submit' value='Submit'>");
+		    			print("</div>");
+		    			//update attendance and/or remove members
+		    			if((isset($_POST["option"]) || isset($_POST["remove"])) && isset($_POST["submit"])){
+		    				$selected = $_POST["option"];
+		    				$removeList = $_POST["remove"];
+			    			if(!empty($selected)){
+			    				foreach ($selected as $attneded) {
+			    					$mysqli->query("UPDATE members SET number_attend = number_attned+1 WHERE members.memberID = '$selected[$i]'");
+			    				}
+			    			}if(!empty($removeList)){
+			    				print("<div class='forms'>Are you sure you want to delete these members? <br>
+			    					<input type='submit' name='imsure value='I'm Sure'>");
+			    				if(isset($_POST["imsure"])){
+			    					foreach ($removeList as $r) {
+			    						$delete = $mysqli->query("DELETE FROM members WHERE memberID=$r");
+			    					}
+			    				}
+			    				
+			    			}
+		    				// ALTER TABLE `members` auto_increment = 3;
+		    			}
 
-			        print( 
-			            "<table> 
-			                <tr>
-			                	<td> $photo </td>
-			                    <td> $f_name </td>
-			                    <td> $l_name </td> 
-			                    <td> $sport </td>
-			                    <td>  <input type='checkbox' name='option[]'' value='$memberID'> In Attendance? </td>
-			                </tr>
-			            </table>");
-			    }
-			    print("<input type='submit' name='submit' value='Submit'>");
-    			print("</div>");
-    			if(isset($_POST["option"]) && isset($_POST["submit"])){
-    				$selected = $_POST["option"];
-    				foreach ($selected as $attneded) {
-    					$mysqli->query("UPDATE members SET number_attend = number_attned+1 WHERE members.memberID = '$selected[$i]'");
-    				}
-    			}
-
-				}
-				else {
-					echo '<p id="welcome_p">This page is still in construction. For more features please <a href="login.php">Login</a></p>';
+				}else {
+					print( 
+				            "<table> 
+				                <tr>
+				                	<td> $photo </td>
+				                    <td> $f_name </td>
+				                    <td> $l_name </td> 
+				                    <td> $sport </td>
+				                    <td> $year </td>
+				                </tr>
+				            </table>");
+		    			print("</div>");
 				}
 			?> 
 		    </div>  <!-- End of gallery_container div -->  	   
