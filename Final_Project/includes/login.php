d5e7d1a2d8844272e21a9e632ed762016c16fac4
nsterling4 <nas95@cornell.edu>
2017-05-16 15:27:37 -0400

    admin login

--- Final_Project/includes/login.php
+++ Final_Project/includes/login.php
@@ -11,6 +11,7 @@
 <body>
 	<?php
 	require_once 'config.php';
+	session_start();
 	?>
 	<div class="contents">
 		<div class ="top_bar"> <!-- Contains header and Nav Bar -->
@@ -22,7 +23,33 @@
 			
 
 			<?php
-    	  		include 'includes/navbar.php';
+    	  		echo 
+			    '<div class="top_bar"> <!-- Contains header and Nav Bar -->
+			        <div class="nav_bar"><!--Bar to navigate around the page-->
+			         <img class="bear_logo" src="images/CornellBigRed.png" alt="Cornell Logo" title="Cornell University"> 
+			            <ul id="main">
+			                <li><a href="index.php">Home</a></li>
+			                <li><a href="articles.php">Articles</a></li>
+			                <li><a href="gallery.php">Gallery</a></li>
+			                <li><a href="events.php">Events</a></li>
+			                <li><a href="members.php">Members</a></li>
+			                <li><a href="about.php">About Us</a></li>
+			                <li><a href="contact.php">Contact Us</a></li>
+			            </ul>
+
+			            <ul class="log" id="desk">
+			               <div class="fb-login-button" data-max-rows="1" data-size="large" data-button-type="login_with" data-show-faces="false" data-auto-logout-link="true" data-use-continue-as="false"></div>
+			            </ul>
+
+			            <ul class="log" id="tab">
+			               <div class="fb-login-button" data-max-rows="1" data-size="medium" data-button-type="login_with" data-show-faces="false" data-auto-logout-link="true" data-use-continue-as="false"></div>
+			            </ul>
+
+			            <ul class="log" id="mob">
+			               <div class="fb-login-button" data-max-rows="1" data-size="small" data-button-type="login_with" data-show-faces="false" data-auto-logout-link="true" data-use-continue-as="false"></div>
+			            </ul>
+			        </div> <!-- End of nav_bar div -->
+			    </div>  <!--End of top_bar div-->';
     		?> 
     	</div> <!-- End of top_bar div -->
 
@@ -114,7 +141,7 @@
 
 		<footer>
 			<?php
-    	  	include 'includes/bottom.php';
+    	  	include 'bottom.php';
     		?> 
 		</footer> <!-- end of footer div -->  
 	</div> <!-- end of contents div -->
